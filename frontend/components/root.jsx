92b78f9282bba47af3d2192503d85bff21ba7680
guocong <ryp0313@gmail.com>
2021-06-01 18:01:59 -0700

    4th commit

---
+++ frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Root extends React.Component{
+    render(){
+        return(
+            <div>Welcome to AirCnC</div>
+        )
+    }
+}
+
+export default Root;
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /(node_modules)/, //ignore these files
         use: {
           loader: 'babel-loader', //the name of the loader we are going to use (babel-loader).
-          query: { // options for the loader
+          options: { // options for the loader
             // for webpack 5, this 'query' key should be 'options' key instead
             // for more info: https://webpack.js.org/configuration/module/#ruleoptions--rulequery
             presets: ['@babel/env', '@babel/react'] //tells loader to use @babel/env which transpiles back to es5 and @babel/react which converts jsx code

a22889c96b444a76ac4ca296567cc7036dfc2373
guocong <ryp0313@gmail.com>
2022-10-12 18:52:50 -0700

    modify home page

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -3933,7 +3933,39 @@ var DisplayReservation = /*#__PURE__*/function (_React$Component) {
         showup: false
       });
       this.props.fetchRental();
-    }
+    } //     showSerch(){
+    //     this.setState({
+    //         search: true,
+    //         reservation: false,
+    //         select_search: "select_search",
+    //         select_reser: "", 
+    //     })
+    // }
+    // showReservation(){
+    //     this.setState({
+    //         search: false,
+    //         reservation: true,
+    //         select_search: "",
+    //         select_reser: "select_reser", 
+    //     })
+    // }
+    // showContent(){
+    //     if (this.state.search === true) {
+    //         return <SearchContainer />
+    //     } else {
+    //         return <DisplayReservation rentals={this.props.rentals} deleteRental={this.props.deleteRental} fetchRental={this.props.fetchRental}/>
+    //     }
+    // }
+    // render(){
+    //     return(
+    //         <div className='homepage-selection'>
+    //             <text className='home-nav-1' id={this.state.select_search} onClick={() => this.showSerch()}>Places to stay</text>
+    //             <text className='home-nav-2' id={this.state.select_reser} onClick={() => this.showReservation()}>Show my reservations</text>
+    //             {this.showContent()}
+    //         </div>
+    //     )
+    // }
+
   }, {
     key: "render",
     value: function render() {

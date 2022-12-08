2449cfb924966e1678df11b0bc0597aef5f3ef5a
guocong <ryp0313@gmail.com>
2022-12-07 16:39:54 -0800

    try split the css

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -2676,7 +2676,17 @@ var HouseIndex = /*#__PURE__*/function (_React$Component) {
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
         className: "search-button",
         type: "submit"
-      }, "Search")))));
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("svg", {
+        viewBox: "0 0 32 32",
+        xmlns: "http://www.w3.org/2000/svg",
+        "aria-hidden": "true",
+        role: "presentation",
+        focusable: "false"
+      }, " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("g", {
+        fill: "none"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("path", {
+        d: "m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"
+      }))))))));
     }
   }, {
     key: "updateLocation",
@@ -3279,7 +3289,18 @@ var Search = /*#__PURE__*/function (_React$Component) {
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__.Link, {
         className: "search-button",
         to: this.inputParams()
-      }, "Search"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br", null));
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("svg", {
+        className: "mainpage-svg",
+        viewBox: "0 0 32 32",
+        xmlns: "http://www.w3.org/2000/svg",
+        "aria-hidden": "true",
+        role: "presentation",
+        focusable: "false"
+      }, " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("g", {
+        fill: "none"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("path", {
+        d: "m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"
+      })))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("br", null));
     }
   }]);
 
--- frontend/components/search/house_index.jsx
+++ frontend/components/search/house_index.jsx
@@ -62,12 +62,17 @@ class HouseIndex extends React.Component{
                             </select>     
                         </div>
                         <div className='search-button-div-2'>
-                            <button className="search-button" type="submit">Search</button>
-                        {/* <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 5.33333; overflow: visible;">
-                            <g fill="none">
-                                <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
-                            </g>   
-                        </svg> */}
+                            {/* <button className="search-button" type="submit">Search</button> */}
+                            
+                            {/* <div class="search-svg" data-icon="true" data-testid="little-search-icon"> */}
+                                <button className="search-button" type="submit">
+                                    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false"> {/* style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 5.33333; overflow: visible;" */}
+                                        <g fill="none">
+                                            <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
+                                        </g>   
+                                    </svg>
+                                </button>
+                            {/* </div> */}
                         </div>
                     </div>
                     
